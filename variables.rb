0209fb1dc85e21e788a62905719a19dc4f7052ee
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-07-09 19:29:43 -0700

    Change strings, add puts

--- variables.rb
+++ variables.rb
@@ -5,8 +5,15 @@ current_age = 40
 years_married = 2
 number_of_siblings = 5
 pocket_change = 0.81
-career_batting_average = .335
-percentage_of_year_over = .5178
+career_batting_average = 0.335
+percentage_of_year_over = 0.5178
 traveled_to_england = false
 traveled_to_costa_rica = true
 completed_data_types_lesson = true
+name = "Gabriel C. Nunez"
+current_city = "Tucson"
+current_occupation = "coding student"
+puts "Hi! My name is #{name}."
+puts "I live in #{current_city} and I am currently a #{current_occupation}."
+puts "I'm #{current_age} years old and have been married for #{years_married} years."
+puts "I have #{number_of_siblings} brothers and sisters, and I think they are the best!"
