7292dcd479554f318a8f9a1ccb464d93b8032f72
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-07-09 19:01:54 -0700

    Add booleans

--- variables.rb
+++ variables.rb
@@ -7,3 +7,6 @@ number_of_siblings = 5
 pocket_change = 0.81
 career_batting_average = .335
 percentage_of_year_over = .5178
+traveled_to_england = false
+traveled_to_costa_rica = true
+completed_data_types_lesson = false
